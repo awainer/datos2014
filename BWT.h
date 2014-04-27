98051f0a3d3f431e12aaf8dd7b44b43df4154908
tavo <tavo@Tavo-Notebook>
2014-04-27 19:31:48 -0300

    Solamente subo el .cpp y .h del BWT para probar el nuevo repo

---
+++ BWT.h
@@ -0,0 +1,22 @@
+/*
+ * BWT.h
+ *
+ *  Created on: 27/04/2014
+ *      Author: tavo
+ */
+
+#ifndef BWT_H
+#define BWT_H
+
+
+class BWT {
+private:
+	int algo=1;
+
+public:
+	BWT();
+	~BWT();
+
+};
+
+#endif // BWT_H
